450c6624c0172b2c2c7c64840724c8f7e59cd70d
Christophe Phillips <c.phillips@uliege.be>
2025-11-25 22:57:35 +0100

    Typos and help

--- hmri_MPRAGElike.m
+++ hmri_MPRAGElike.m
@@ -1,29 +1,31 @@
 function fn_out = hmri_MPRAGElike(fn_in,params)
 
 % Function to calculate an MPRAGE-like image from a set of (2 or 3) images
-% acquired with the MPM protocol. 
-% This procedure is directly derived from M-A Fortin's work and is only a 
+% acquired with the MPM protocol.
+% This procedure is directly derived from M-A Fortin's work and is only a
 % Matlab reimplemenation of his work. Check the reference here under.
-% 
+%
 % FORMAT
 % fn_out = hmri_MPRAGElike(fn_in,params)
-% 
+%
 % INPUT
-% fn_in : char array of filenames of 2 or 3 input images, 
-%           1st one should be T1w (numerator), 
+% fn_in : char array of filenames of 2 or 3 input images,
+%           1st one should be T1w (numerator),
 %           2nd (+3rd if provided) should be MTw and/or PDw (denominator)
 % params : structure with some parameters
 %   .lambda : regularisation parameter(s) [100, def]
-%             If several values are passed, i.e. in a vector, then 1 image 
-%             is created per value. These are labdeled 'l100' and 'l200' 
+%             If several values are passed, i.e. in a vector, then 1 image
+%             is created per value. These are labdeled 'l100' and 'l200'
 %             for lambada 100 and 200 for example.
-%   .indiv  : a binary flag, to decide whether individual images are 
+%   .indiv  : a binary flag, to decide whether individual images are
 %             created for each 2nd and 3rd input filename when 3 images are
 %             passed in fn_in. These images will be labelled 'i1' and 'i2'.
 %            [false, def.]
+%   .thresh : threshold for [min max]range in MPRAGE-like image as in paper
+%             but if left empty, NO thresholding applied. [[0 500], def.]
 %   .BIDSform : a binary flag, to indicate if BIDS format is followed.
 %               [false, def.]
-% 
+%
 % OUTPUT
 % fn_out : char array of filenames of generated image(s)
 %          Depending on the input parameters there could be up to 3 images
@@ -35,13 +37,14 @@ function fn_out = hmri_MPRAGElike(fn_in,params)
 %_______________________________________________________________________
 % Copyright (C) 2025 Cyclotron Research Centre
 
-% Written by C. Phillips, 
+% Written by C. Phillips,
 % Cyclotron Research Centre, University of Liege, Belgium
 
 % Set defaults & check input
 params_def = struct(...
     'lambda', 100, ...
     'indiv', false, ...
+    'thresh', [0 500], ...
     'BIDSform', false);
 if nargin<2, params = params_def; end
 
@@ -54,7 +57,7 @@ end
 if Nimg_in==2 && params.indiv
     params.indiv = false ; % No individual images if only 2 input images
 end
-    
+
 % Prepare output filenames and check nr of files to be created
 Nlambda = numel(params.lambda);
 if Nlambda>1
@@ -71,6 +74,11 @@ fn_tmp = fullfile(pth_out, fn_basename);
 % get the job done
 ic_flags = struct('dmtx',true);
 
+% Prepare output volume(s)
+V_out = V_in(1);
+V_out.dt(1) = 16; % use floats!
+V_out.descrip = 'MPRAGE-like image';
+% get the job done
 for ii=1:Nlambda
    if Nlambda==1 % just one lambda
        fn_out_ii = [fn_tmp,'_MPRAGEl.nii'];
@@ -91,8 +99,9 @@ for ii=1:Nlambda
         vval_MPRlike(vval_MPRlike>params.thresh(2)) = 0;
     end
     spm_write_vol(V_out,reshape(vval_MPRlike',sz(1:3)));
+    
 end
 
-
+fn_out = fn_out_ii;
 
 end
