355f88730d0e6f5118bc90d845be4c9c444d3870
Christophe Phillips <c.phillips@uliege.be>
2025-12-05 16:19:45 +0100

    Checking SCAIFIELD data

--- MPRAGElike_script.m
+++ MPRAGElike_script.m
@@ -9,15 +9,25 @@
 
 % Pick up some "old" test data
 pth_data_top = 'D:\ccc_DATA\Test_MPRAGElike';
+
 % pth_data_MS7T = fullfile( pth_data_top , 'MS7T');
 % fn_T1w = spm_select('FPList',pth_data_MS7T,'^sub.*-T1w_.*_MPM\.nii$');
 % fn_PDw = spm_select('FPList',pth_data_MS7T,'^sub.*-PDw_.*_MPM\.nii$');
 % fn_MTw = spm_select('FPList',pth_data_MS7T,'^sub.*-MTw_.*_MPM\.nii$');
 
+% IRONSLEEP data
 pth_data_IRONS = fullfile( pth_data_top , 'IRONSLEEP');
 fn_T1w = spm_select('FPList',pth_data_IRONS,'^sub.*-T1w_.*_MPM\.nii$');
 fn_PDw = spm_select('FPList',pth_data_IRONS,'^sub.*-PDw_.*_MPM\.nii$');
 fn_MTw = spm_select('FPList',pth_data_IRONS,'^sub.*-MTw_.*_MPM\.nii$');
+
+% SCAIFIELD data
+% CRC
+pth_data_SF = fullfile( pth_data_top,'CRC','SCAIFIELD','Pilot-04_S1','NIfTI');
+fn_T1w = spm_select('FPList',pth_data_SF,'^s.*-0012-.*-01-.*\.nii$');
+fn_PDw = spm_select('FPList',pth_data_SF,'^s.*-0014-.*-01-.*\.nii$');
+fn_MTw = spm_select('FPList',pth_data_SF,'^s.*-0016-.*-01-.*\.nii$');
+
 
 % fn_in =char(fn_T1w,fn_MTw);
 fn_in =char(fn_T1w,fn_MTw,fn_PDw);
@@ -57,7 +67,11 @@ v_global = spm_global(spm_vol(fn_in));
 %   390.5394
 %   479.7623
 %   476.1641
-
+% v_global = % SCAIFIELD
+%   716.4834
+%   706.7789
+%   756.9217
+  
 % Check range, mean/mode, histogram
 val_in = spm_read_vols(spm_vol(fn_in));
 sz = size(val_in)
@@ -67,21 +81,26 @@ mean_all = mean(vval_in)
 %   194.5161  123.1467  146.8916
 % mean_all =  % IRONSLEEP
 %   192.3433  222.7650  229.1573
-thr_global = mean_all*.8
+% mean_all =  % SCAIFIELD
+%   342.9494  353.9259  356.7661
+
+  thr_global = mean_all*.8
 % thr_global =  % MS7T
 %   155.6129   98.5174  117.5133
 % -> quite OK but some extra bits for T1w...
 % thr_global = % IRONSLEEP
 %   153.8746  178.2120  183.3258
-
+% thr_global = % SCAIFIELD
+%   274.3595  283.1407  285.4129
 
 % Crete a mask based on the union of the 3 thresholded images
+fn_mask = spm_file(Vmsk.fname,'suffix','_mask');
 mask_glob = vval_in(:,1)>thr_global(1);
 for ii=2:numel(thr_global)
     mask_glob = mask_glob | vval_in(:,ii)>thr_global(ii);
 end
 Vmsk = spm_vol(fn_in(1,:));
-Vmsk.fname = spm_file(Vmsk.fname,'suffix','_mask');
+Vmsk.fname = fn_mask;
 Vmsk.dt(1) = 2;
 spm_write_vol(Vmsk,reshape(mask_glob,sz(1:3)))
 
@@ -97,6 +116,8 @@ subplot(3,2,2), histogram(vval_in(mask_glob,1))
 subplot(3,2,4), histogram(vval_in(mask_glob,2))
 subplot(3,2,6), histogram(vval_in(mask_glob,3))
 
+spm_check_registration(fn_in)
+
 % Check median values
 % All voxels
 median_all = median(vval_in)
@@ -104,15 +125,20 @@ median_all = median(vval_in)
 %     45    36    38 % -> pretty useless !
 % median_all = % IRONSLEEP 
 %     22    20    24 % -> pretty useless !
-    
-mediam_msk = [median(vval_in(mask_glob,1)) median(vval_in(mask_glob,2)) median(vval_in(mask_glob,3))]
+% median_all =  % SCAIFIELD
+%     34    41    34    
+
+    mediam_msk = [median(vval_in(mask_glob,1)) median(vval_in(mask_glob,2)) median(vval_in(mask_glob,3))]
 % mediam_msk = % MS7T
 %    552   334   436 % Seems very interesting!
 % mediam_msk = % IRONSLEEP 
 %    470   553   581
+% mediam_msk = % SCAIFIELD
+%          963         956        1054
 
 mean(mediam_msk)
 % 534.6667 % IRONSLEEP 
+% 991 % SCAIFIELD -> take it divided by 10 ???
 
 % Check range as percentile
 prctile(vval_in,[5 95])
@@ -120,12 +146,16 @@ prctile(vval_in,[5 95])
 %    813   525   650
 %            2           2           2 % IRONSLEEP 
 %          776         982        1001
+%            5           5           5 % SCAIFIELD
+%         1555        1525        1621 
 
 prctile([vval_in(mask_glob,1) vval_in(mask_glob,2) vval_in(mask_glob,3)],[5 95])
 %          162          64          66 % MS7T
 %         1033         685         848
 %          154          80          86 % IRONSLEEP 
 %          971        1256        1275
+%          151         286         150
+%         1940        1851        1988 SCAIFIELD
 
 %% Lambda validation ?
 % Check Structural Similarity Index Metric (SSIM)
